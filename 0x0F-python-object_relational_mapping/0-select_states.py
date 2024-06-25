c5f34e2a5d8a949fa55a57c4a4aae2dea6549b6f
Olaoluwa-cyber <oladipovictor007@gmail.com>
2024-06-25 14:19:01 -0700

    b

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
 """ Script that lists all states from the database hbtn_0e_0_usa """
+
 import MySQLdb
 from sys import argv
 
@@ -10,8 +12,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
 
-    # It gives us the ability to have multiple seperate working environments
-    # through the same connection to the database.
+    # It gives us the ability to have multiple seperate working environments through the same connection to the database.
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
 
